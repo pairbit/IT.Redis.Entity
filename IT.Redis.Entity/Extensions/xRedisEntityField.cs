49c9da2d2e023db6e89b69bf4e89221236eb7d91
Ivan Tikhonov <itikhonov@mail.com>
2024-05-18 14:07:40 +0300

    refactoring

--- IT.Redis.Entity/Extensions/xRedisEntityField.cs
+++ IT.Redis.Entity/Extensions/xRedisEntityField.cs
@@ -2,20 +2,6 @@
 
 public static class xRedisEntityField
 {
-    public static TEntity? GetEntity<TEntity, IEntity>(this RedisEntityField<IEntity> field, in RedisValue value) where TEntity : IEntity, new()
-    {
-        if (value.IsNull) return default;
-
-        var entity = new TEntity();
-
-        field.Write(entity, in value);
-
-        return entity;
-    }
-
-    public static TEntity? GetEntity<TEntity>(this RedisEntityField<TEntity> field, in RedisValue value) where TEntity : new()
-        => field.GetEntity<TEntity, TEntity>(in value);
-
     public static TEntity? GetEntity<TEntity, IEntity>(this RedisEntityField<IEntity> field, in RedisValue value, Func<TEntity> newEntity) where TEntity : IEntity
     {
         if (value.IsNull) return default;
@@ -29,4 +15,10 @@ public static class xRedisEntityField
 
     public static TEntity? GetEntity<TEntity>(this RedisEntityField<TEntity> field, in RedisValue value, Func<TEntity> newEntity)
         => field.GetEntity<TEntity, TEntity>(in value, newEntity);
+
+    public static TEntity? GetEntity<TEntity, IEntity>(this RedisEntityField<IEntity> field, in RedisValue value) where TEntity : IEntity, new()
+        => field.GetEntity(in value, static () => new TEntity());
+
+    public static TEntity? GetEntity<TEntity>(this RedisEntityField<TEntity> field, in RedisValue value) where TEntity : new()
+        => field.GetEntity<TEntity, TEntity>(in value);
 }
