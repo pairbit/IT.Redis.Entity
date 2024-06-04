e3430e99d9f3df2de6b0cfebdba5312d4e64e249
Ivan Tikhonov <itikhonov@mail.com>
2024-06-04 13:59:44 +0300

    redis localhost:6379

--- IT.Redis.Entity.Tests/Const.cs
+++ IT.Redis.Entity.Tests/Const.cs
@@ -3,5 +3,5 @@
 internal static class Const
 {
     public static readonly string Connection = 
-        "localhost:6382,defaultDatabase=0,syncTimeout=5000,allowAdmin=False,connectTimeout=5000,ssl=False,abortConnect=False";
+        "localhost:6379,defaultDatabase=0,syncTimeout=5000,allowAdmin=False,connectTimeout=5000,ssl=False,abortConnect=False";
 }
